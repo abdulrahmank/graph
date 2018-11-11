614e781e091e2577be227032a524e100dd43b087
Abdul Rahman <kadkab.abdul@gmail.com>
2018-11-12 03:27:37 +0530

    Move creating graph to common method

--- root/commons.go
+++ root/commons.go
@@ -10,3 +10,22 @@ type Node struct {
 type Edge struct {
 	To *Node
 }
+
+func CreateGraph(adjacencyList map[string]NodeList) map[string]*Node {
+	Graph := make(map[string]*Node)
+	for key, value := range adjacencyList {
+		if Graph[key] == nil {
+			Graph[key] = &Node{Value: key}
+		}
+		if len(Graph[key].Edges) == 0 {
+			Graph[key].Edges = make([]Edge, 0, 10)
+		}
+		for _, toNode := range value {
+			if Graph[toNode] == nil {
+				Graph[toNode] = &Node{Value: toNode}
+			}
+			Graph[key].Edges = append(Graph[key].Edges, Edge{To: Graph[toNode]})
+		}
+	}
+	return Graph
+}
--- root/recursion/dfs.go
+++ root/recursion/dfs.go
@@ -3,26 +3,9 @@ package recursion
 import commons "github.com/abdulrahmank/graph/root"
 
 func Dfs(root string, adjacencyList map[string]commons.NodeList) commons.NodeList {
-	Graph := make(map[string]*commons.Node)
-	for key, value := range adjacencyList {
-		if Graph[key] == nil {
-			Graph[key] = &commons.Node{Value: key}
-		}
-		if len(Graph[key].Edges) == 0 {
-			Graph[key].Edges = make([]commons.Edge, 0, 10)
-		}
-		for _, toNode := range value {
-			if Graph[toNode] == nil {
-				Graph[toNode] = &commons.Node{Value: toNode}
-			}
-			Graph[key].Edges = append(Graph[key].Edges, commons.Edge{To: Graph[toNode]})
-		}
-	}
-
+	Graph := commons.CreateGraph(adjacencyList)
 	result := commons.NodeList{}
-
 	dfs(Graph[root], &result)
-
 	return result
 }
 
